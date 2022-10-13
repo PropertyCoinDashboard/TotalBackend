7fa7bbf58dcc2ee46f3adfa54fc801893a392454
sky81219 <=>
2022-10-13 17:45:51 +0900

    architectre start

---
+++ backend/config/settings/dev.py
@@ -0,0 +1,15 @@
+from .common import *
+
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["http://0.0.0.0:8080", "http://127.0.0.1:8000"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_DATABASE'),
+        'USER': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_ROOT_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
+    }
+}
