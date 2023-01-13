1cb533da3be463a1b5d42dd49cbfe09051fab3f2
sky81219 <=>
2023-01-13 23:01:24 +0900

    최적화

--- backend/coin/apis.py
+++ backend/coin/apis.py
@@ -11,7 +11,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.mixins import DestroyModelMixin
 from rest_framework.generics import (
-    CreateAPIView, ListAPIView, RetrieveAPIView
+    CreateAPIView, ListAPIView
 )
 
 from .serializer import (
