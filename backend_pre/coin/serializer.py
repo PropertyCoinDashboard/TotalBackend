634a17af0afd407209ea3d660d067e8460a5a22b
sky81219 <=>
2023-06-04 16:32:38 +0900

    refactor: architecture

--- backend_pre/coin/serializer.py
+++ backend_pre/coin/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from dashboaring.models import CoinSymbolCoinList, TestOR
+from dashboaring.models import CoinSymbolCoinList
 
 
 class CoinViewExistSerializer(serializers.ModelSerializer):
